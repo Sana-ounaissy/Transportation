9b60392c681b65ce1e126d62aa09f65828c02a74
KhalilAbbaass <103119609+KhalilAbbaass@users.noreply.github.com>
2022-05-08 16:07:33 +0300

    deleting a row from drivers table LS

--- admindriver.js
+++ admindriver.js
@@ -9,13 +9,30 @@ for(let i=0; i<users.length; i++){
                 printdrivershere.innerHTML += "<tr> <td>" + users[i].fname + "</td>" 
                             + "<td>"+ users[i].lname + "</td>" 
                             + "<td>"+ users[i].em + "</td>" 
-                            + "<td>"+ users[i].ph+ "</td>"
+                            + "<td>"+ users[i].ph + "</td>"
                             + "<td>"+ users[i].birth + "</td>" 
-                            + "<td>"+ users[i].gen + "</td> </tr>";
+                            + "<td>"+ users[i].gen + "</td>"
+                            + '<td> <button onclick="Delete()" class="btn btn-outline-dark" type="button">Delete</button> </td>'
+                            + "</tr>";
+  
+
+ }
+  
+  function Delete(){
+
+      let td = event.target.parentNode; 
+      let tr = td.parentNode; // the row to be removed
+      tr.parentNode.removeChild(tr);
+     localStorage.removeItem(JSON.stringify(users[1]));
+     localStorage.removeItem(content);   
+
+     
     }
 
 
 }
+
+
 
 
 

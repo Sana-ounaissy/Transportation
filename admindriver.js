b818acc2e82ae4efd4f74d2cb42b7016957f7eed
KhalilAbbaass <103119609+KhalilAbbaass@users.noreply.github.com>
2022-05-09 12:11:36 +0300

    admindriver

--- admindriver.js
+++ admindriver.js
@@ -1,19 +1,18 @@
-let printdrivershere = document.getElementById("tbodyadmindriver");
-let users = JSON.parse(localStorage.getItem("allEntries"));
-console.log(users[0]);
+let printdrivershere = document.getElementById(" tbodyadmindriver ");
+let users = JSON.parse(localStorage.getItem(" allEntries "));
 
     
-for(let i=0; i<users.length; i++){
+for(let i=0; i<users.length;i++){
    
-     if(users[i].selcted =='Driver'){
-                printdrivershere.innerHTML += "<tr> <td>" + users[i].fname + "</td>" 
-                            + "<td>"+ users[i].lname + "</td>" 
-                            + "<td>"+ users[i].em + "</td>" 
-                            + "<td>"+ users[i].ph + "</td>"
-                            + "<td>"+ users[i].birth + "</td>" 
-                            + "<td>"+ users[i].gen + "</td>"
-                            + ' <td> <button onclick="Delete()" class="btn btn-outline-dark" type="button">Delete</button> </td> '
-                            + "</tr>";
+     if(users[i].selcted ==' Driver '){
+                printdrivershere.innerHTML += "<tr> <td>" + users[i].fname + "</td> " 
+                            + "<td>"+ users[i].lname + "</td> " 
+                            + "<td>"+ users[i].em + "</td> " 
+                            + "<td>"+ users[i].ph + "</td> "
+                            + "<td>"+ users[i].birth + "</td> " 
+                            + "<td>"+ users[i].gen + "</td> "
+                            + ' <td> <button onclick="Delete()" class="btn btn-outline-dark" type="button">Delete</button> </td>'
+                            + "</tr> ";
   
 
  }
@@ -23,7 +22,6 @@ for(let i=0; i<users.length; i++){
       let td = event.target.parentNode; 
       let tr = td.parentNode; // the row to be removed
       tr.parentNode.removeChild(tr);
-      
     }
 
 
@@ -34,5 +32,3 @@ for(let i=0; i<users.length; i++){
 
 
 
-
-// printdrivershere.innerHTML = localStorage.getItem(user)

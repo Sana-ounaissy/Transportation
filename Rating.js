3a70064646323b996260ba4a4565073e19c38270
Sana-ounaissy <85550893+Sana-ounaissy@users.noreply.github.com>
2022-05-11 22:39:13 +0300

    editing to rating and pricing

--- Rating.js
+++ Rating.js
@@ -1,8 +1,9 @@
 let printdrivershere = document.getElementById("rowdriverrating");
 let users = JSON.parse(localStorage.getItem("allEntries"));
 console.log(users[0]);
-
-    
+let r=document.getElementsByClassName("rating");
+let z=document.getElementsByClassName("rating-label");    
+let rat=document.getElementsByClassName("my-rating");
 for(let i=0; i<users.length; i++){
    
      if(users[i].selcted =='Driver'){
@@ -17,28 +18,18 @@ for(let i=0; i<users.length; i++){
 
                  +'       <div class="rate bg-dark py-3 text-white mt-3">'
                  +'           <h6 class="mb-0">Rate your driver</h6>'
-                 +'           <div class="rating">'
-                 +'               <input type="radio" name="rating" value="5" id="5">'
-                 +'               <label for="5">☆</label>'
-                 +'               <input type="radio" name="rating" value="4" id="4">'
-                 +'               <label for="4">☆</label>'
-                  +'              <input type="radio" name="rating" value="3" id="3">'
-                  +'              <label for="3">☆</label>'
-                  +'              <input type="radio" name="rating" value="2" id="2">'
-                  +'              <label for="2">☆</label>'
-                  +'              <input type="radio" name="rating" value="1" id="1">'
-                 +'               <label for="1">☆</label>'
-                +'            </div>'
+                 
                 +'            <div class="buttons px-4 mt-0"> <button'
                +'                     class="btn btn-warning btn-block rating-submit">Submit</button>'
                +'             </div>'
               +'          </div>'
               +'      </div>'
               +'  </div>'
-  
 
+
+            
  }
-  
-
+ 
+              
 }
 

7ad3b83bcfd2370648236724990cbd6d36344dc3
Sana-ounaissy <85550893+Sana-ounaissy@users.noreply.github.com>
2022-05-12 15:37:24 +0300

    edited

--- message.js
+++ message.js
@@ -1,5 +1,6 @@
 let printmessageshere = document.getElementById("tbodymessages");
 let Users = JSON.parse(localStorage.getItem("allMessages"));
+
 console.log(Users.length);
 
     
@@ -15,4 +16,22 @@ for(let i=0; i<Users.length; i++){
 
 }
 
+
+let card=document.getElementById("container");
+let dr = JSON.parse(localStorage.getItem('allChats'));
+console.log(dr.length);
+for(let i=0; i<dr.length; i++){
+    card.innerHTML+= ` <div class="card" data-label="In Progress">
+    <div class="card__container">
+        <h1 class="card__header" id="driverName">
+    
+      </h1>
+      <p class="card__body" id ="card">
+       `+ dr[i].liveChat +`
+      </p>  
+    </div>
+  </div>
+ </div>`
+}
+let drname=document.getElementById("driverName");
 
--- tripinfo.js
+++ tripinfo.js
@@ -30,3 +30,23 @@ for(let i=0; i<trips.length; i++){
                              </tr>`
 
 }
+
+function driverChat(){
+var allChats = [];
+allChats.push(chat);
+        // Parse any JSON previously stored in allEntries
+     var existingChats = JSON.parse(localStorage.getItem("allChats"));
+        if(existingChats == null) existingChats = [];
+       
+        var chat = {
+           "liveChat":document.getElementById("Mess").value,
+         
+            };
+        localStorage.setItem("chat", JSON.stringify(chat));
+        // Save allEntries back to local storage
+        existingChats.push(chat);
+        localStorage.setItem('allChats', JSON.stringify(existingChats));
+let dr = JSON.parse(localStorage.getItem('chat'));
+console.log(dr.liveChat);}
+
+
